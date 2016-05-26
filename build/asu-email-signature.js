1c8e888ea4918ff5078b29d7f2e8c2c7b57e1e8a
Sebastian Nievas <sebinievas@gmail.com>
2016-05-26 08:04:23 -0700

    Fix phone number check

--- build/asu-email-signature.js
+++ build/asu-email-signature.js
@@ -1,28 +1,37 @@
 angular.module('emailSignatureBranding', [])
   .controller('Controller', ['$scope', function($scope) {
-    $scope.name = 'Full Name';
-    $scope.title = '';
+    $scope.name       = 'Full Name';
+    $scope.title      = '';
     $scope.university = 'Arizona State University';
-    $scope.unit = 'College of Liberal Arts and Sciences';
-    $scope.address1 = 'P.O. Box 876505';
-    $scope.address2 = '';
-    $scope.address3 = 'Tempe, AZ 85287-6505';
-    $scope.phone = '';
-    $scope.mobile = '';
+    $scope.unit       = 'College of Liberal Arts and Sciences';
+    $scope.address1   = 'P.O. Box 876505';
+    $scope.address2   = '';
+    $scope.address3   = 'Tempe, AZ 85287-6505';
+    $scope.phone      = '';
+    $scope.mobile     = '';
 
     $scope.checkMobile = function() {
-      $scope.mobile = $scope.mobile_input != ''
-        ? '| C: ' + $scope.mobile_input
-        : '';
+      $scope.mobile = '';
 
-      if ($scope.phone == '') {
-        $scope.mobile = $scope.mobile.replace(/^\| /, '');
+      if ($scope.mobile_input) {
+        $scope.mobile = '| C: ' + $scope.mobile_input;
+
+        if ($scope.phone == undefined || $scope.phone == '') {
+          $scope.mobile = $scope.mobile.replace(/^\| /, '');
+        }
       }
     }
 
     $scope.checkPhone = function() {
-      $scope.phone = $scope.phone_input != ''
-        ? 'P: ' + $scope.phone_input
-        : '';
+      $scope.phone = '';
+
+      if ($scope.phone_input) {
+        $scope.phone = 'P: ' + $scope.phone_input;
+      }
+    }
+
+    $scope.checkPhoneNumbers = function() {
+      $scope.checkPhone();
+      $scope.checkMobile();
     }
   }]);
